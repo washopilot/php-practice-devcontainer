136e3805eb7f58ae517dda8dee3d03083378b7b9
Fernando Chicaiza <washopilot@yahoo.com>
2025-04-03 14:30:15 +0000

    adding practice.php

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
 <body>
     <?php
-    var_dump($_COOKIE);
-    phpinfo();
-
-
-    ?>
+    include "practice.php"
+        ?>
 </body>
+
+</html>
---
+++ practice.php
@@ -0,0 +1,12 @@
+<?php
+for ($i = 1; $i <= 5; $i++) {
+    echo "Número: $i<br>";
+}
+
+$contador = 3;
+while ($contador > 0) {
+    echo "Cuenta atrás: $contador<br>";
+    $contador--;
+}
+
+
