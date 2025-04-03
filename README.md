e93aa22c1b77ff56ac6ceb37b1cfeb4844fa1697
Fernando Chicaiza <washopilot@yahoo.com>
2025-04-03 14:23:13 +0000

    add browser sync

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ejecutar en la terminal
+
+``` 
+browser-sync start --proxy "localhost:8080" --files "**/*.php, **/*.css, **/*.js"
+``` 
--- index.php
+++ index.php
@@ -1,12 +1,8 @@
-<?php
+<body>
+    <?php
+    var_dump($_COOKIE);
+    phpinfo();
 
-$a = 10;
-$b = 20;
 
-$suma = $a + $b;
-$resta = $a - $b;
-$producto = $a * $b;
-$division = $a / $b;
-$modulo = $a % $b;
-
-echo "Suma: $suma, Resta: $resta, Multiplicación: $producto, División: $division, Módulo: $modulo<br>";
+    ?>
+</body>
