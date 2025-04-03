526dfbd0ff4fd3371b67a3900c683618867731d5
Fernando Chicaiza <washopilot@yahoo.com>
2025-04-03 07:59:18 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+$suma = $a + $b;
+$resta = $a - $b;
+$producto = $a * $b;
+$division = $a / $b;
+$modulo = $a % $b;
+
+echo "Suma: $suma, Resta: $resta, Multiplicación: $producto, División: $division, Módulo: $modulo<br>";
